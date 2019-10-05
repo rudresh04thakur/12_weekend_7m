e69f21ef5d489fb2c86a8511d3dc828ac47f1c2e
Gopal <gopal.thakur@edkal.com>
2019-10-05 11:29:40 +0530

    Update

--- client2/src/app/front/front.module.ts
+++ client2/src/app/front/front.module.ts
@@ -8,12 +8,14 @@ import { HeaderComponent } from './partial/header/header.component';
 import { FooterComponent } from './partial/footer/footer.component';
 import { FrontComponent } from './front.component';
 import { ProductsComponent } from './products/products.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [HomeComponent, AboutComponent, HeaderComponent, FooterComponent, FrontComponent, ProductsComponent],
   imports: [
     CommonModule,
-    FrontRoutingModule
+    FrontRoutingModule,
+    HttpClientModule
   ]
 })
 export class FrontModule { }
--- client2/src/app/front/products/products.component.ts
+++ client2/src/app/front/products/products.component.ts
@@ -8,11 +8,13 @@ import { ProductsService } from '../services/products.service'
 })
 export class ProductsComponent implements OnInit {
   imagePath = "../../../../assets/";
+  imagePath_server = "http://localhost:3000/"
   constructor(private _ar:ActivatedRoute,private _r:Router,private _ser: ProductsService) { }
-
+  products;
   ngOnInit() {
     this._ser.getList('all').subscribe((res)=>{
       console.log("product list =>",res);
+      this.products = res;
     })
     //console.log("Category ========== ",this._ar.snapshot.params.cat); 
 
